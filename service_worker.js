e9b87c838c8ce1b9b95d277b58d11fa1ab6304e4
ranmd9a <ranmd9a@abox3.so-net.ne.jp>
2022-04-22 01:30:13 +0900

    Added process to destroy ObjectUrl.

--- service_worker.js
+++ service_worker.js
@@ -4,6 +4,9 @@ const updateContextMenus = async () => {
 	chrome.contextMenus.create({
 		id: "create-playlist",
 		title: "検索結果から playlist を作成",
+		documentUrlPatterns: [
+			"https://beatsaver.com/*"
+		],
 		contexts: ["action"],
 	});
 };
@@ -15,15 +18,28 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 		console.warn(`not target`);
 		return;
 	}
+	const options = {
+		frameId: 0, // メインframeにのみ送信
+	};
 	switch (info.menuItemId) {
 		case 'create-playlist':
-			const response = chrome.tabs.sendMessage(tab.id, 'create-playlist')
+			const response = chrome.tabs.sendMessage(tab.id, { id: 'create-playlist' }, options);
+
 			response.then((result) => {
+				const downloadCallback = (downloadId) => {
+					if (downloadId == null) {
+						// downloadId が undefined の場合失敗している。
+						// https://developer.chrome.com/docs/extensions/reference/downloads/#method-download
+						console.warn(`download failed.`);
+						// 続行
+					}
+					// 使用した OjbectURL を破棄
+					chrome.tabs.sendMessage(tab.id, { id: 'revoke-object-url', url: result.url }, options);
+				};
 				chrome.downloads.download({
 					url: result.url,
-					// filename: "search_result.bplist",
 					filename: result.filename,
-				});
+				}, downloadCallback);
 			}).catch((error) => {
 				console.error(error);
 			})
